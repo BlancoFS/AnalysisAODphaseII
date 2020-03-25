47392868d2be1a4940e25a25c3bb7d2e1242fdc8
BlancoFS <60780075+BlancoFS@users.noreply.github.com>
2020-03-25 11:14:55 +0100

    Update ExamAnalyzer.h

--- plugins/ExamAnalyzer.h
+++ plugins/ExamAnalyzer.h
@@ -62,6 +62,115 @@ class ExampleMuonAnalyzer2: public edm::EDAnalyzer {
   edm::EDGetTokenT<reco::VertexCollection>                      vtxToken;
   edm::EDGetTokenT<reco::BeamSpot>                              beamSpotToken;
   
+  TH1F* h_dxy;
+  TH1F* h_dz;
+  TH1F* h_vxy;
+  TH1F* h_vz;
+  TH1F* h_vr;
+  TH1F* h_pt;
+
+  TH2F* hGenMuons_vxy_vz;
+  TH2F* hStaMuons_vxy_vz;
+  TH2F* hTrkMuons_vxy_vz;
+  TH2F* hGlbMuons_vxy_vz;
+  TH2F* hTightMuons_vxy_vz;
+  TH2F* hSoftMuons_vxy_vz;
+  TH2F* hStaMuons_noGen_vxy_vz;
+  TH2F* hTrkMuons_noGen_vxy_vz;
+  TH2F* hGlbMuons_noGen_vxy_vz;
+  TH2F* hTightMuons_noGen_vxy_vz;
+  TH2F* hSoftMuons_noGen_vxy_vz;
+
+  TH1F* hGenMuons_vxy;
+  TH1F* hStaMuons_vxy;
+  TH1F* hTrkMuons_vxy;
+  TH1F* hGlbMuons_vxy;
+  TH1F* hTightMuons_vxy;
+  TH1F* hSoftMuons_vxy;
+  TH1F* hStaMuons_noGen_vxy;
+  TH1F* hTrkMuons_noGen_vxy; 
+  TH1F* hGlbMuons_noGen_vxy; 
+  TH1F* hTightMuons_noGen_vxy; 
+  TH1F* hSoftMuons_noGen_vxy; 
+
+  TH1F* hGenMuons_vz;
+  TH1F* hStaMuons_vz;
+  TH1F* hTrkMuons_vz;
+  TH1F* hGlbMuons_vz;
+  TH1F* hTightMuons_vz;
+  TH1F* hSoftMuons_vz;
+  TH1F* hStaMuons_noGen_vz;
+  TH1F* hTrkMuons_noGen_vz; 
+  TH1F* hGlbMuons_noGen_vz; 
+  TH1F* hTightMuons_noGen_vz; 
+  TH1F* hSoftMuons_noGen_vz; 
+
+  TH1F* hGenMuons_vr;
+  TH1F* hStaMuons_vr;
+  TH1F* hTrkMuons_vr;
+  TH1F* hGlbMuons_vr;
+  TH1F* hTightMuons_vr;
+  TH1F* hSoftMuons_vr;
+  TH1F* hStaMuons_noGen_vr;
+  TH1F* hTrkMuons_noGen_vr; 
+  TH1F* hGlbMuons_noGen_vr; 
+  TH1F* hTightMuons_noGen_vr; 
+  TH1F* hSoftMuons_noGen_vr; 
+
+  TH1F* hGenMuons_eta;
+  TH1F* hStaMuons_eta;
+  TH1F* hTrkMuons_eta;
+  TH1F* hGlbMuons_eta;
+  TH1F* hTightMuons_eta;
+  TH1F* hSoftMuons_eta;
+  TH1F* hStaMuons_noGen_eta;
+  TH1F* hTrkMuons_noGen_eta;
+  TH1F* hGlbMuons_noGen_eta;
+  TH1F* hTightMuons_noGen_eta;
+  TH1F* hSoftMuons_noGen_eta;
+
+  TH1F* hGenMuons_phi;
+  TH1F* hStaMuons_phi;
+  TH1F* hTrkMuons_phi;
+  TH1F* hGlbMuons_phi;
+  TH1F* hTightMuons_phi;                                                                                                           
+  TH1F* hSoftMuons_phi;
+  TH1F* hStaMuons_noGen_phi;    
+  TH1F* hTrkMuons_noGen_phi;
+  TH1F* hGlbMuons_noGen_phi;                                                                                                        
+  TH1F* hTightMuons_noGen_phi;
+  TH1F* hSoftMuons_noGen_phi;
+
+  TH1F* hGenMuons_pt;
+  TH1F* hStaMuons_pt;
+  TH1F* hTrkMuons_pt;
+  TH1F* hGlbMuons_pt;
+  TH1F* hTightMuons_pt;                                                                                                             
+  TH1F* hSoftMuons_pt;
+  TH1F* hStaMuons_noGen_pt;                                                                                            
+  TH1F* hTrkMuons_noGen_pt;                                                                                                         
+  TH1F* hGlbMuons_noGen_pt;                                                                                                         
+  TH1F* hTightMuons_noGen_pt;                                                                                                          
+  TH1F* hSoftMuons_noGen_pt;
+
+  TH1F* hStaMuons_dR;
+  TH1F* hTrkMuons_dR;
+  TH1F* hGlbMuons_dR;
+  TH1F* hTightMuons_dR;                                                                                                            
+  TH1F* hSoftMuons_dR;
+
+  TH2F* hStaMuons_pt_resolution  [nbins_pt];
+  TH2F* hTrkMuons_pt_resolution  [nbins_pt];
+  TH2F* hGlbMuons_pt_resolution  [nbins_pt];
+  TH2F* hTightMuons_pt_resolution[nbins_pt];
+  TH2F* hSoftMuons_pt_resolution [nbins_pt];
+
+  TH1F* hMuPFChargeIso; 
+  TH1F* hMuPFNeutralIso;
+  TH1F* hMuPFPhotonIso; 
+  TH1F* hMuPFPUIso;   
+  TH1F* hMuPFIso;
+  
   
 };
 #endif
